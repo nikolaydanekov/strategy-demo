0c3446c8cd431b6775afba1f81a1e10ca84752fc
nikolaydanekov <73487258+nikolaydanekov@users.noreply.github.com>
2021-10-15 12:06:19 +0300

    Refactored old ugly code in a traditional way

--- src/main/java/com/patterns/strategy/service/NameFilterStrategy.java
+++ src/main/java/com/patterns/strategy/service/NameFilterStrategy.java
@@ -1,9 +1,9 @@
 package com.patterns.strategy.service;
 
-
 import com.patterns.strategy.domain.DemoEntity;
 
 public class NameFilterStrategy implements  FilterStrategy{
+
     @Override
     public boolean shouldFilter(DemoEntity demoEntity, String value) {
         return demoEntity.getAmount().equals(Long.valueOf(value));
