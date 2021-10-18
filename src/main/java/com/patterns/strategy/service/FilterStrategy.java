86243c4cd831858d723531d5f04ab519f78145b1
nikolaydanekov <73487258+nikolaydanekov@users.noreply.github.com>
2021-10-18 13:14:39 +0300

    Renamed package

--- src/main/java/com/patterns/strategy/service/FilterStrategy.java
+++ src/main/java/com/patterns/strategy/service/FilterStrategy.java
@@ -3,7 +3,5 @@ package com.patterns.strategy.service;
 import com.patterns.strategy.domain.DemoEntity;
 
 public interface FilterStrategy {
-
-
     boolean shouldFilter(DemoEntity demoEntity, String value);
 }
--- src/main/java/com/patterns/strategy/service/StatusFilterStrategy.java
+++ src/main/java/com/patterns/strategy/service/StatusFilterStrategy.java
@@ -4,8 +4,6 @@ import com.patterns.strategy.domain.DemoEntity;
 import com.patterns.strategy.domain.Status;
 
 public class StatusFilterStrategy implements  FilterStrategy{
-
-
     @Override
     public boolean shouldFilter(DemoEntity demoEntity, String value) {
         return demoEntity.getStatus().equals(Status.valueOf(value));
