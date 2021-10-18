a6294d45620a806290958d3f9b9944945e084a75
nikolaydanekov <73487258+nikolaydanekov@users.noreply.github.com>
2021-10-18 13:14:39 +0300

    Renamed package

--- src/main/java/com/patterns/strategy/service/AmountFilterStrategy.java
+++ src/main/java/com/patterns/strategy/service/AmountFilterStrategy.java
@@ -3,10 +3,8 @@ package com.patterns.strategy.service;
 import com.patterns.strategy.domain.DemoEntity;
 
 public class AmountFilterStrategy implements  FilterStrategy{
-
-
     @Override
     public boolean shouldFilter(DemoEntity demoEntity, String value) {
-        return demoEntity.getName().equals(value);
+        return demoEntity.getAmount().equals(Long.valueOf(value));
     }
 }
--- src/main/java/com/patterns/strategy/service/NameFilterStrategy.java
+++ src/main/java/com/patterns/strategy/service/NameFilterStrategy.java
@@ -3,10 +3,8 @@ package com.patterns.strategy.service;
 import com.patterns.strategy.domain.DemoEntity;
 
 public class NameFilterStrategy implements  FilterStrategy{
-
-
     @Override
     public boolean shouldFilter(DemoEntity demoEntity, String value) {
-        return demoEntity.getAmount().equals(Long.valueOf(value));
+        return demoEntity.getName().equals(value);
     }
 }
