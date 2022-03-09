c10af0c9fdfd7027fbda694f988844519db3ab15
NikolayDanekov <nikolaydanekov@coherentSolutions.com>
2022-03-09 22:02:12 +0200

    SonarLint optimization

--- src/main/java/com/patterns/strategy/service/FilterServiceImpl.java
+++ src/main/java/com/patterns/strategy/service/FilterServiceImpl.java
@@ -3,10 +3,17 @@ package com.patterns.strategy.service;
 import com.patterns.strategy.domain.VideoGame;
 import com.patterns.strategy.dto.FilterByEnum;
 import com.patterns.strategy.repository.VideoGameRepository;
-import com.patterns.strategy.service.strategy.*;
+import com.patterns.strategy.service.strategy.FilterStrategy;
+import com.patterns.strategy.service.strategy.GenreFilterStrategy;
+import com.patterns.strategy.service.strategy.PriceGreaterThanFilterStrategy;
+import com.patterns.strategy.service.strategy.PriceLowerThanFilterStrategy;
+import com.patterns.strategy.service.strategy.RatingGreaterThanFilterStrategy;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 public class FilterServiceImpl implements FilterService{
@@ -16,7 +23,7 @@ public class FilterServiceImpl implements FilterService{
 
     public FilterServiceImpl(VideoGameRepository demoEntityRepository){
         this.videoGameRepository = demoEntityRepository;
-        filterStrategyMap = new HashMap<>();
+        filterStrategyMap = new EnumMap<>(FilterByEnum.class);
         filterStrategyMap.put(FilterByEnum.PRICE_GREATER_THAN, new PriceGreaterThanFilterStrategy());
         filterStrategyMap.put(FilterByEnum.PRICE_LOWER_THAN, new PriceLowerThanFilterStrategy());
         filterStrategyMap.put(FilterByEnum.GENRE, new GenreFilterStrategy());
